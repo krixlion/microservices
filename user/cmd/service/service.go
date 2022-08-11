2b81230eb24a7b8e6d87c50ade727a2c045d4a4e
Krixlion <konstanty.cyran.k@gmail.com>
2022-08-11 13:38:50 +0200

    User-service logging

--- user/cmd/service/service.go
+++ user/cmd/service/service.go
@@ -29,6 +29,7 @@ func (s *UserHandler) Index(_ *pb.UserFilter, stream pb.User_IndexServer) error 
 		Email: "email",
 		Phone: "phone lol",
 	})
+	logger.Log("msg", "Access index method", "transport", "gRPC", "port", port)
 	if err != nil {
 		return err
 	}
