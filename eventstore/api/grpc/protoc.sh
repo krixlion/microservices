bb9777d3250fb4172c1d37f45962cd2d5e99deb9
Krixlion <konstanty.cyran.k@gmail.com>
2022-09-07 19:41:02 +0200

    Protoc script fix

--- eventstore/api/grpc/protoc.sh
+++ eventstore/api/grpc/protoc.sh
@@ -1,7 +1,7 @@
 #! /bin/sh.
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 DIR="$(realpath "${DIR}")"
-GO_PKG_PATH="$(cd $DIR/../../internal/pb/ && pwd)"
+GO_PB_PATH="$(cd $DIR/../../internal/pb/ && pwd)"
 
-protoc --go_out=paths=source_relative:$GO_PKG_PATH --go-grpc_out=paths=source_relative:$GO_PKG_PATH -I $DIR event_store.proto
-protoc-go-inject-tag -input="$GO_PKG_PATH/*.pb.go"
+protoc --go_out=paths=source_relative:$GO_PB_PATH --go-grpc_out=paths=source_relative:$GO_PB_PATH -I $DIR event_store.proto
+protoc-go-inject-tag -input="$GO_PB_PATH/*.pb.go"
